e84f8223852d607c9d7b1373740a699d2463191b
taka <jj4068851@gmail.com>
2025-07-23 04:04:57 +0000

    feat: center page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,27 @@
+"use client"
+
 import { ConnectButton } from "@/components/ConnectButton";
 import Image from 'next/image';
 import { CreateContractButton } from "@/components/CreateContractButton";
 
 export default function Home() {
-
   return (
-    <div className={"pages"}>
-      <Image src="/reown.svg" alt="Reown" width={150} height={150} priority />
-      <h1>AppKit ethers Next.js App Router Example</h1>
-
-      <ConnectButton />
-      <CreateContractButton/>
-      <div className="advice">
-        <p>
-          This projectId only works on localhost. <br/>
-          Go to <a href="https://cloud.reown.com" target="_blank" className="link-button" rel="Reown Cloud">Reown Cloud</a> to get your own.
-        </p>
+  <div className="pages">
+      <div className="connect-button-wrapper">
+        <ConnectButton />
       </div>
-    </div>
+      <div className="center-container">
+        <Image src="/reown.svg" alt="Reown" width={150} height={150} priority />
+        <h1>AppKit ethers Next.js App Router Example</h1>
+      </div>
+      <CreateContractButton />
+      <div className="center-container">
+          <p>
+            This projectId only works on localhost. <br />
+            Go to <a href="https://cloud.reown.com" target="_blank" className="link-button" rel="Reown Cloud">Reown Cloud</a> to get your own.
+          </p>
+      </div>
+  </div>
+
   );
 }
