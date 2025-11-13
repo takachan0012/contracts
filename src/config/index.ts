1b043569287a1c460d24f0ca6052c4818cc27b41
takachan0012 <jj4068851@gmail.com>
2025-11-13 05:13:13 +0000

    feat: define chain atlantic testnet

--- src/config/index.ts
+++ src/config/index.ts
@@ -33,6 +33,31 @@ export const pharos_testnet = defineChain({
   },
 })
 
+export const pharos_testnet_atlantic = defineChain({
+  id: 688689,
+  name: "Atlantic Testnet",
+  caipNetworkId: "eip155:688689",
+  chainNamespace: "eip155",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Atlantic Testnet",
+    symbol: "PHRS",
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://atlantic.dplabs-internal.com/"],
+      webSocket: ["wss://testnet.dplabs-internal.com/"],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "Explorer",
+      url: "https://atlantic.pharosscan.xyz/",
+    },
+  },
+});
+
+
 export const nexus_testnet3 = defineChain({
   id: 3940,
   name: 'Nexus Testnet III',
@@ -57,5 +82,9 @@ export const nexus_testnet3 = defineChain({
   },
 })
 
-export const networks:[AppKitNetwork, ...AppKitNetwork[]] = [pharos_testnet, nexus_testnet3]
+export const networks: [AppKitNetwork, ...AppKitNetwork[]] = [
+  pharos_testnet,
+  pharos_testnet_atlantic,
+  nexus_testnet3,
+];
 export const ethersAdapter = new EthersAdapter();
